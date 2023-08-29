230bf7fd3b137e27474c958ec6fe4fe63fd84aa2
Giordano Temple <giordanotemple@gmail.com>
2023-08-28 21:55:39 -0400

    fix

--- 3d-engine/DemoViewer.java
+++ 3d-engine/DemoViewer.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 
 public class DemoViewer {
